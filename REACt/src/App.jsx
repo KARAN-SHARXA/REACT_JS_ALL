223fae1270071a2d9a136ad3d6708b119f18e5e6
KARAN-SHARXA <karansharma2302578@gmail.com>
2025-11-03 22:35:03 +0530

    fragement,map,codition randring,props

---
+++ REACt/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Fragement from './assets/componenet/Fragement'
+
+function App() {
+
+  return (
+    <div>
+      <Fragement name="karan" age="23" ></Fragement>
+    </div>
+  )
+}
+
+export default App
